fca1d962f1afa46096636bd63aa5db3f709f5d49
Vlad <71086815+swixge@users.noreply.github.com>
2020-09-10 19:03:27 +0300

    kursfinal

---
+++ KURSOVA_original/KURSOVA_original/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace KURSOVA_original
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Color_Click = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.pictureBox2.Location = new System.Drawing.Point(513, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(256, 426);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(539, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введіть кординати прямокутника ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(539, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Кординати початку";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Location = new System.Drawing.Point(628, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Location = new System.Drawing.Point(539, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Кординати кінця";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.Location = new System.Drawing.Point(539, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.Location = new System.Drawing.Point(539, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label7.Location = new System.Drawing.Point(628, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Y";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.Color_Click);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(526, 191);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 143);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Заливка";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label9.Location = new System.Drawing.Point(6, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(220, 32);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Виберіть колір заливки, та натисніть на фігуру щоб зафарбувати її";
+            // 
+            // Color_Click
+            // 
+            this.Color_Click.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Color_Click.Location = new System.Drawing.Point(18, 82);
+            this.Color_Click.Name = "Color_Click";
+            this.Color_Click.Size = new System.Drawing.Size(194, 45);
+            this.Color_Click.TabIndex = 13;
+            this.Color_Click.Text = "Вибір кольору";
+            this.Color_Click.UseVisualStyleBackColor = false;
+            this.Color_Click.Click += new System.EventHandler(this.Choose_Color);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(542, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Намалювати ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(559, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(63, 21);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(651, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(63, 21);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(559, 116);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(63, 21);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(651, 116);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(63, 23);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.GrayText;
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(657, 425);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "By Vlad Khvorostenko";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ЗАЛИВКА";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Color_Click;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ KURSOVA_original/KURSOVA_original/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KURSOVA_original
+{
+    public partial class Form1 : Form
+    {   
+
+        Bitmap bmp;
+        Graphics g;
+        Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            int a = e.X;     //Знаходження кординат початкової точки заливки!
+            int b = e.Y;
+            int maxpixels = 1680 * 1050;
+            int[] Mx = new int[maxpixels];
+            int[] My = new int[maxpixels];
+
+            Color startColor = bmp.GetPixel(a, b);
+
+            int N = 0;                                              //найденное число внутренних элементов.
+            int Ntek = 0;                                           //текущий анализируемый элемент
+
+            bmp.SetPixel(a, b, Color.Red);
+            Mx[Ntek] = a;
+            My[Ntek] = b;
+
+
+            for (int i = 0; i < maxpixels; i++)
+            {
+
+                if (b > 0 && bmp.GetPixel(a, b - 1).Equals(startColor))
+                {
+                 
+                    bmp.SetPixel(a, b - 1, Color.Red);
+                    N += 1;
+                    Ntek += 1;
+                    Mx[Ntek] = a;
+                    My[Ntek] = b - 1;
+
+                }
+
+
+                if (a + 1 < bmp.Width && bmp.GetPixel(a + 1, b).Equals(startColor))
+                {
+               
+                    bmp.SetPixel(a + 1, b, Color.Red);
+                    N += 1;
+                    Ntek += 1;
+                    Mx[Ntek] = a + 1;
+                    My[Ntek] = b;
+
+                }
+
+
+
+                if (b + 1 < bmp.Height && bmp.GetPixel(a, b + 1).Equals(startColor))
+                {
+                
+                    bmp.SetPixel(a, b + 1, Color.Red);
+                    N += 1;
+                    Ntek += 1;
+                    Mx[Ntek] = a;
+                    My[Ntek] = b + 1;
+
+                }
+
+                if (a > 0 && bmp.GetPixel(a - 1, b).Equals(startColor))
+                {
+                   
+                    bmp.SetPixel(a - 1, b, Color.Red);
+                    N += 1;
+                    Ntek += 1;
+                    Mx[Ntek] = a - 1;
+                    My[Ntek] = b;
+
+                }
+
+
+                if (i + 1 <= Ntek)
+                {
+                    a = Mx[i + 1];
+                    b = My[i + 1];
+                }
+                else
+                {
+                    MessageBox.Show(string.Format("Ваша фигура зафарбована; {0} steps made", i));
+                    break;
+                }
+
+            }
+
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+        private void Draw( float x1, float y1, float x2, float y2)
+        {
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pen = new Pen(Color.Black, 8);
+            g.DrawRectangle(pen, x1, y1, x2, y2);
+            pictureBox1.Image = bmp;
+
+            
+
+        }   // Function draw rectangle
+
+       
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float x1;
+            float y1;
+            float x2;
+            float y2;
+
+            x1 = Convert.ToSingle(textBox1.Text);
+            y1 = Convert.ToSingle(textBox2.Text);
+            x2 = Convert.ToSingle(textBox3.Text);
+            y2 = Convert.ToSingle(textBox4.Text);
+            Draw(x1, y1, x2, y2);
+        }  // Draw button!
+
+       
+
+         private void Choose_Color(object sender, EventArgs e)
+         {
+            DialogResult colorResult = colorDialog1.ShowDialog();    //Отображает форму как модальное диалоговое окно.
+
+            if (colorResult == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+         }    //background color*
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' ) && (e.KeyChar <= '9')) 
+            {
+                return;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',')
+            {
+                if (textBox1.Text.IndexOf(',') != 1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+
+
+        }           //character check
+
+        
+    }
+}
